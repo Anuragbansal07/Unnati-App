7f762d1ae40e16e513960b2e60bb642934c3ff74
Anuragbansal07 <anu9988185114@gmail.com>
2024-09-12 16:05:10 +0530

    first push

--- app/index.tsx
+++ app/index.tsx
@@ -1,67 +1,67 @@
-// // // index.tsx
-// import React from 'react';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createStackNavigator } from '@react-navigation/stack';
-// import LanguageScreen from './screens/LanguageScreen';
-// import SignUpScreen from './screens/SignUpScreen';
-// import OtpVerificationScreen from './screens/OtpVerificationScreen';
-// import SignInScreen from './screens/SignInScreen';
-// import SignInUpScreen from './screens/SignInUpScreen';
-// import WelcomeScreen from './screens/WelcomeScreen';
-// import WhoAreYouScreen from './screens/WhoAreYouScreen';
-// import TopicsOfInterestScreen from './screens/TopicsOfInterestScreen';
-// import NotificationReminderScreen from './screens/NotificationReminderScreen';
+// // index.tsx
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LanguageScreen from './screens/LanguageScreen';
+import SignUpScreen from './screens/SignUpScreen';
+import OtpVerificationScreen from './screens/OtpVerificationScreen';
+import SignInScreen from './screens/SignInScreen';
+import SignInUpScreen from './screens/SignInUpScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+import WhoAreYouScreen from './screens/WhoAreYouScreen';
+import TopicsOfInterestScreen from './screens/TopicsOfInterestScreen';
+import NotificationReminderScreen from './screens/NotificationReminderScreen';
 
-// const Stack = createStackNavigator();
+const Stack = createStackNavigator();
 
-// export default function Index() {
-//   return (
-//     // <NavigationContainer>
-//       <Stack.Navigator initialRouteName='LanguageScreen'>
-//         <Stack.Screen name="Language" component={LanguageScreen} options={{headerShown:false}} />
-//         <Stack.Screen name="SignUpScreen" component={SignUpScreen} options={{headerShown:false}} />
-//         <Stack.Screen name="OtpVerificationScreen" component={OtpVerificationScreen} options={{headerShown:false}} />
-//         <Stack.Screen name="SignInScreen" component={SignInScreen} options={{headerShown:false}} />
-//         <Stack.Screen name="SignInUpScreen" component={SignInUpScreen} options={{headerShown:false}} />
+export default function Index() {
+  return (
+    // <NavigationContainer>
+      <Stack.Navigator initialRouteName='LanguageScreen'>
+        <Stack.Screen name="Language" component={LanguageScreen} options={{headerShown:false}} />
+        <Stack.Screen name="SignUpScreen" component={SignUpScreen} options={{headerShown:false}} />
+        <Stack.Screen name="OtpVerificationScreen" component={OtpVerificationScreen} options={{headerShown:false}} />
+        <Stack.Screen name="SignInScreen" component={SignInScreen} options={{headerShown:false}} />
+        <Stack.Screen name="SignInUpScreen" component={SignInUpScreen} options={{headerShown:false}} />
 
-//         <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} options={{headerShown:false}} />
-//         <Stack.Screen name="WhoAreYouScreen" component={WhoAreYouScreen} options={{headerShown:false}} />
-//         <Stack.Screen name="TopicsOfInterestScreen" component={TopicsOfInterestScreen} options={{headerShown:false}} />
-//         <Stack.Screen name="NotificationReminderScreen" component={NotificationReminderScreen} options={{headerShown:false}} />
-
-
-//       </Stack.Navigator>
-//     // </NavigationContainer>
-//   );
-// }
+        <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} options={{headerShown:false}} />
+        <Stack.Screen name="WhoAreYouScreen" component={WhoAreYouScreen} options={{headerShown:false}} />
+        <Stack.Screen name="TopicsOfInterestScreen" component={TopicsOfInterestScreen} options={{headerShown:false}} />
+        <Stack.Screen name="NotificationReminderScreen" component={NotificationReminderScreen} options={{headerShown:false}} />
 
 
-
-
-import React, { Component } from "react";
-import SignInScreen from "./screens/SignInScreen";
-import LanguageScreen from "./screens/LanguageScreen";
-import SignUpScreen from "./screens/SignUpScreen";
-import OtpVerificationScreen from "./screens/OtpVerificationScreen";
-import WelcomeScreen from "./screens/WelcomeScreen";
-import WhoAreYouScreen from "./screens/WhoAreYouScreen";
-import TopicsOfInterestScreen from "./screens/TopicsOfInterestScreen";
-import SignInUpScreen from "./screens/SignInUpScreen";
-import OtpCheckScreen from "./screens/OtpCheckScreen";
-export default class index extends Component {
-  render() {
-    return (
-      // <LanguageScreen/>
-      // <SignInScreen/>
-      // <SignUpScreen/>
-      // <OtpVerificationScreen/>
-      // <WelcomeScreen/>
-      // <WhoAreYouScreen/>
-      // <SignInUpScreen/>
-      <OtpCheckScreen/>
-      // <TopicsOfInterestScreen/>
-    );
-  }
+      </Stack.Navigator>
+    // </NavigationContainer>
+  );
 }
+
+
+
+
+// import React, { Component } from "react";
+// import SignInScreen from "./screens/SignInScreen";
+// import LanguageScreen from "./screens/LanguageScreen";
+// import SignUpScreen from "./screens/SignUpScreen";
+// import OtpVerificationScreen from "./screens/OtpVerificationScreen";
+// import WelcomeScreen from "./screens/WelcomeScreen";
+// import WhoAreYouScreen from "./screens/WhoAreYouScreen";
+// import TopicsOfInterestScreen from "./screens/TopicsOfInterestScreen";
+// import SignInUpScreen from "./screens/SignInUpScreen";
+// import OtpCheckScreen from "./screens/OtpCheckScreen";
+// export default class index extends Component {
+//   render() {
+//     return (
+//       // <LanguageScreen/>
+//       // <SignInScreen/>
+//       // <SignUpScreen/>
+//       // <OtpVerificationScreen/>
+//       // <WelcomeScreen/>
+//       // <WhoAreYouScreen/>
+//       // <SignInUpScreen/>
+//       <OtpCheckScreen/>
+//       // <TopicsOfInterestScreen/>
+//     );
+//   }
+// }
 
 
--- app/screens/LanguageScreen.tsx
+++ app/screens/LanguageScreen.tsx
@@ -1,164 +1,7 @@
-// import { Text, StyleSheet, View, Dimensions } from 'react-native';
-// import React, { Component } from 'react';
-// import RNPickerSelect from 'react-native-picker-select';
-// import { GestureHandlerRootView, TouchableOpacity } from "react-native-gesture-handler";
-// import Icon from 'react-native-vector-icons/FontAwesome';
-// const { width, height } = Dimensions.get("window");
-// import { firebase } from '@react-native-firebase/auth';
-// export default class LanguageScreen extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       selectedLanguage: null,
-//     };
-//   }
-//   handleContinue = () => {
-//     const { selectedLanguage } = this.state;
-
-//     if (selectedLanguage) {
-//       // Navigate to SignUpScreen with the selected language
-//       // console.log(firebase.apps);
-//       this.props.navigation.navigate('SignUpScreen', { language: selectedLanguage });
-//     } else {
-//       // Handle the case when no language is selected, e.g., show an alert
-//       console.log('No language selected');
-//     }
-//   };
-
-//   handleLanguageChange = (value) => {
-//     this.setState({ selectedLanguage: value });
-//     console.log('Selected language:', value);
-//   };
-
-//   render() {
-//     const languageOptions = [
-//       { label: 'English', value: 'en' },
-//       { label: 'Spanish', value: 'es' },
-//       { label: 'French', value: 'fr' },
-//       // Add more languages as needed
-//     ];
-
-//     return (
-//         <GestureHandlerRootView style={{flex:1}}> 
-//       <View style={styles.outerContainer}>
-//         <View style={styles.container}>
-//           <Text style={styles.language1}>भाषा चुने</Text>
-//           <Text style={styles.language2}>Select Language</Text>
-
-//           <RNPickerSelect
-//             placeholder={{ label: '--Select Language--', value: null }}
-//             onValueChange={this.handleLanguageChange}
-//             items={languageOptions}
-//             style={pickerSelectStyles}
-//             value={this.state.selectedLanguage}
-//           />
-//           <TouchableOpacity style={styles.continue} onPress={this.handleContinue}>
-//           <View style={styles.continueContainer}>
-//             <Text style={styles.continueInside}>Continue </Text>
-//             <Icon name="arrow-right" size={17} color="#fff" style={styles.icon} />
-//           </View>
-//           </TouchableOpacity>
-
-//         </View>
-//       </View>
-//          </GestureHandlerRootView> 
-
-//         // }
-//     );
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   outerContainer: {
-//     backgroundColor: '#f5f5f5',
-//     flex: 1,
-//   },
-//   container: {
-//     height: height * (189 / 777),
-//     width: width * (350 / 393),
-//     top: height * (296 / 777),
-//     left: width * (21 / 393),
-//     backgroundColor: '#f5f5f5',
-//     justifyContent: 'center',
-//   },
-//   language1: {
-//     fontWeight: '400',
-//     fontSize: 20,
-//     lineHeight: 26.6,
-//     height: height * (27 / 777),
-//   },
-//   language2: {
-//     fontWeight: 'bold',
-//     fontSize: 20,
-//     lineHeight: 24.2,
-//     height: height * (24 / 777),
-//     marginBottom: 20,
-//   },
-//   continue: {
-//     marginTop: 40,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     height: height * (40 / 777),
-//     width: width * (330 / 393),
-//     backgroundColor: '#003366',
-//     borderWidth: 1,
-//     borderRadius: 8,
-//   },
-//   continueInside: {
-//     fontWeight: 'bold',
-//     fontSize: 16,
-//     lineHeight: 19.36,
-//     color: '#f5f5f5',
-//   },
-//   continueContainer : {
-//     flex : 1,
-//     flexDirection : 'row',
-//     alignItems : 'center',
-//     justifyContent : 'center',
-//   },
-//   icon : {
-//     width : 24,
-//     height : 24,
-//     alignItems : 'center',
-//     justifyContent : 'center',
-//     marginLeft : 5,
-//     paddingVertical : 3,
-//   }
-// });
-
-// const pickerSelectStyles = StyleSheet.create({
-//   inputIOS: {
-//     fontSize: 16,
-//     paddingHorizontal: 10,
-//     paddingVertical: 12,
-//     borderWidth: 1,
-//     borderColor: '#ccc',
-//     borderRadius: 5,
-//     color: '#000',
-//     paddingRight: 30,
-//   },
-//   inputAndroid: {
-//     height : height*(44/777),
-//     width : width*(350/393),
-//     fontSize: 16,
-//     // paddingHorizontal: 10,
-//     // paddingVertical: 8,
-//     borderWidth: 5,
-
-//     borderColor: 'red',
-//     borderRadius: 5,
-//     color: '#fff',
-//     // paddingRight: 30,
-//   },
-// });
-
-
-
 import { Text, StyleSheet, View, Dimensions ,Image} from 'react-native';
 import React, { Component } from 'react';
 import DropDownPicker from 'react-native-dropdown-picker';
 import { GestureHandlerRootView, TouchableOpacity } from "react-native-gesture-handler";
-import Icon from 'react-native-vector-icons/FontAwesome';
 const { width, height } = Dimensions.get("window");
 
 export default class LanguageScreen extends Component {
@@ -166,11 +9,10 @@ export default class LanguageScreen extends Component {
     super(props);
     this.state = {
       selectedLanguage: null,
-      open: false, // Controls dropdown open/close
+      open: false,
       items: [
         { label: 'हिन्दी', value: 'hi' },
         { label: 'English', value: 'en' },
-        // Add more languages as needed
       ],
     };
   }
@@ -179,11 +21,9 @@ export default class LanguageScreen extends Component {
     const { selectedLanguage } = this.state;
 
     if (selectedLanguage) {
-      // Navigate to SignUpScreen with the selected language
       this.props.navigation.navigate('SignInUpScreen', { language: selectedLanguage });
     } else {
       alert("No language selected");
-      // console.log('No language selected');
     }
   };
 
@@ -227,13 +67,11 @@ export default class LanguageScreen extends Component {
               textStyle={styles.labelStyle} 
             />
 
-            {/* Automatically moves the Continue button down when the dropdown expands */}
             <View style={{ marginTop: this.state.open ? 110 : 40 }}>
               <TouchableOpacity style={styles.continue} onPress={this.handleContinue}>
                 <View style={styles.continueContainer}>
                   <Text style={styles.continueInside}>Continue </Text>
                   <Image source={require('../../assets/images/Arrow_right.png')} style={styles.image}/>
-                  {/* <Icon name="arrow-right" size={17} color="#fff" style={styles.icon} /> */}
                 </View>
               </TouchableOpacity>
             </View>
@@ -254,9 +92,7 @@ const styles = StyleSheet.create({
     left: width * (21 / 393),
     height : height*(189/777),
     top : height * (296/777),
-    // alignSelf: 'center',
     backgroundColor: '#f5f5f5',
-    // justifyContent: 'center',
   },
   language1: {
     fontWeight: '400',
@@ -275,11 +111,6 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#333',
     borderRadius: 6,
-
-    // paddingHorizontal : 10,
-    // paddingVertical : 16,
-    // alignItems : 'stretch',
-    // color : "#474747",
   },
   dropdownContainer: {
     width: width * (350 / 393),
@@ -287,8 +118,6 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#333',
     marginBottom : 0,
-    // paddingHorizontal : 10,
-    // paddingVertical : 17,
   },
   placeholderStyle : {
     fontSize : 14,
@@ -332,14 +161,6 @@ const styles = StyleSheet.create({
     width : 24,
     height : 24,
   },
-  // icon : {
-  //   width : 24,
-  //   height : 24,
-  //   alignItems : 'center',
-  //   justifyContent : 'center',
-  //   marginLeft : 5,
-  //   paddingVertical : 3,
-  // },
 
 });
 
